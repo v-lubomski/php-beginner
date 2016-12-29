44783b44a5b6cbe73b3bb370c3184e846b2d7b58
VLubomski <jiogvocal@mail.ru>
2016-12-29 22:10:37 +0400

    lesson03

---
+++ htdocs/home03/function.php
@@ -0,0 +1,23 @@
+<?php
+error_reporting(E_ALL);
+ini_set('log_errors', "On");
+ini_set('display_errors', "Off");
+ini_set('error_log', './error.log');
+
+function myfunc()
+{
+    if (isset($_GET['a']) && isset($_GET['b'])) {
+        $a = $_GET["a"];
+        $b = $_GET["b"];
+        if (floatval($a) && floatval($b)) {
+            $divide = $a / $b;
+            return round($divide, 2, PHP_ROUND_HALF_EVEN);
+        } else {
+            return "Wrong arguments";
+        }
+    } else {
+        return "Wrong arguments";
+    }
+}
+
+echo myfunc();
